f6f3c5266c7786314c3f56a4e685254e369625a9
Toya_Okumura <toya_okumura@nuco.co.jp>
2026-01-29 10:07:57 +0900

    0128分

---
+++ riversi0128/controller.py
@@ -0,0 +1,35 @@
+from model import ReversiModel
+
+
+class ReversiController:
+    EMPTY = ReversiModel.EMPTY
+    BLACK = ReversiModel.BLACK
+    WHITE = ReversiModel.WHITE
+
+    def __init__(self):
+        """
+        初期化
+        """
+        self.rm = ReversiModel()
+        self.turn = self.WHITE
+
+
+    def put(self, x: int,y: int)->None:
+        """
+        (x,y)にコマを置く
+        """
+        x += 1
+        y += 1
+        if self.rm.put(10 * y + x,self.turn):
+            self.turn = self.BLACK if self.turn == self.WHITE else self.WHITE
+
+
+    @property
+    def board(self) -> tuple[tuple[int]]:
+        """
+        盤面を渡す
+        盤面には[X][Y]でアクセスする
+        """
+
+        bd = [self.rm.board[10 * i + 1:10 * i + 9] for i in range(1,9)]
+        return tuple(zip(*bd))
